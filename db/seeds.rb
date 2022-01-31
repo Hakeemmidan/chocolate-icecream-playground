517818993362b6a46d8d0a470d9e6eb9916f2a36
Abdulhakeem Almidan <Hakeemmidan@gmail.com>
2022-02-01 01:48:44 +0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Seed started'
+product = Product.create!(title: 'Programming Ruby 1.9 & 2.0')
+review = product.reviews.create!(rating: 5, body: 'Great book')
+review = product.reviews.create!(rating: 4, body: 'Good book')
+review = product.reviews.create!(rating: 3, body: 'Not so good book')
+review = product.reviews.create!(rating: 2, body: 'Bad book')
+review = product.reviews.create!(rating: 1, body: 'Very bad book')
+review = product.reviews.create!(rating: 0, body: "Don't even bother")
+puts 'Seed ended'
